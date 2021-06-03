c1f9d6dd652e709ea7c46a2d132c04086e4d6b3e
sathiraju chaitanya <sathirajuchaitanya@gmail.com>
2021-06-03 12:08:36 +0530

    added hash true in roteing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     HomeComponent,
     TopNavComponent
-    
   ],
   imports: [
     BrowserModule,
--- src/app/shared/interfaces/emi-calculato.interface.ts
+++ src/app/shared/interfaces/emi-calculato.interface.ts
@@ -1,12 +1,12 @@
-export interface IEmiCalculator{
-    monthlyIncome:number;
-    monthlyExpenses:number;
-    repaymentenure:number;
-    existingloans:boolean;
-    emiExistingLoan:number;
-    eligibiltyLoan:IeligibilityLoan;
+export interface IEmiCalculator {
+    monthlyIncome: number;
+    monthlyExpenses: number;
+    repaymentenure: number;
+    existingloans: boolean;
+    emiExistingLoan: number;
+    eligibiltyLoan: IeligibilityLoan;
 }
-export interface IeligibilityLoan{
-    loanAmount:number;
-    montlyEmi:number;
+export interface IeligibilityLoan {
+    loanAmount: number;
+    montlyEmi: number;
 }
