249b466110d09050a780b69f70c14273cf9f8cb6
sathiraju chaitanya <sathirajuchaitanya@gmail.com>
2021-06-03 12:08:58 +0530

    added hash tru in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes =   [
-  {path:'', redirectTo:'/home', pathMatch:'full'},
-{path:'home',component:HomeComponent}];
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+{path: 'home', component: HomeComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
